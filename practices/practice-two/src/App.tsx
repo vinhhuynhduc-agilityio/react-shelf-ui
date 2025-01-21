c75f7c00d56821e0f349e4604a7f39c8254accca
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-01-21 15:52:13 +0700

    Init project #56

---
+++ practices/practice-two/src/App.tsx
@@ -0,0 +1,8 @@
+const App: React.FC = () => {
+
+  return (
+    <>My book shelf</>
+  );
+};
+
+export default App;
---
+++ practices/practice-two/tailwind.config.js
@@ -0,0 +1,6 @@
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+
+export const plugins = [];
