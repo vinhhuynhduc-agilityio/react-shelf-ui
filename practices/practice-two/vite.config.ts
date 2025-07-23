d324f96155b5fce2d395d163ea5da8fdab2eed69
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-07-23 09:02:06 +0700

    update vite config

--- practices/practice-two/vite.config.ts
+++ practices/practice-two/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Base alias for `src`
-      '@types': path.resolve(__dirname, 'src/types') // Alias for `src/types`
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"), // Base alias for `src`
+			"@types": path.resolve(__dirname, "src/types"), // Alias for `src/types`
+		},
+	},
+	esbuild: {
+		target: "es2020",
+	},
 });
