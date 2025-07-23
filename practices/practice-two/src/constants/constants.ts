bd346e91c76e599c4cc970c8df99e0a1d098bdc9
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-07-23 14:34:12 +0700

    Fix code style

--- practices/practice-two/src/constants/constants.ts
+++ practices/practice-two/src/constants/constants.ts
@@ -1,10 +1,12 @@
 const DEFAULT_AVATAR =
 	"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'%3E%3Crect width='100%25' height='100%25' rx='8' fill='%23ccc'/%3E%3Ccircle cx='32' cy='24' r='12' fill='%23eee'/%3E%3Cpath d='M16 52c0-8 8-12 16-12s16 4 16 12' fill='%23eee'/%3E%3C/svg%3E";
+
 const searchOptions = [
 	{ label: "Title", key: "title" },
 	{ label: "Author", key: "author" },
 	{ label: "Subjects", key: "subjects" },
 ];
+
 const profileOptions = [
 	{ label: "Profile", key: "account-setting" },
 	{ label: "Favourite", key: "favourite" },
--- practices/practice-two/src/constants/queryKeys.ts
+++ practices/practice-two/src/constants/queryKeys.ts
@@ -1,6 +1,8 @@
 export const QUERY_KEY_MY_SHELF = (userId: string) => ["my-shelf", userId];
+
 export const QUERY_KEY_MY_FAVOURITE = (userId: string) => [
 	"my-favourite",
 	userId,
 ];
+
 export const QUERY_KEY_BOOKS = ["books"];
