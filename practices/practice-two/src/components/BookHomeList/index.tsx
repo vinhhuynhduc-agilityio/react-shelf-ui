536aaff3bb5fe68e45306a94e8867c99cf4ca134
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-08-05 11:33:01 +0700

    Refactor code

--- practices/practice-two/src/components/BookHomeList/index.tsx
+++ practices/practice-two/src/components/BookHomeList/index.tsx
@@ -43,53 +43,52 @@ const BookHomeList: React.FC<BookListProps> = ({
     [navigate]
   );
 
-  const renderContent = () => {
-    if (isError) {
-      return (
-        <ApiErrorNotice
-          title="Failed to load books data"
-          errors={[errorMessage]}
-        />
-      );
-    }
-
-    if (isLoading) {
-      return Array.from({ length: skeletonCount }).map((_, idx) => (
-        <div key={idx} className="flex-shrink-0">
-          <Skeleton
-            width={160}
-            height={260}
-            borderRadius={16}
-            additionalClasses="p-4"
-          />
-        </div>
-      ));
-    }
-
-    if (books.length > 0) {
-      return books.map((book) => (
-        <div key={book.id} className="flex-shrink-0">
-          <BookItem {...book} onClick={handleBookClick} />
-        </div>
-      ));
-    }
-
-    return (
-      <ParagraphMessage
-        text="No books available."
-        className="mt-4 text-gray-600 col-span-full"
-      />
-    );
-  };
-
   return (
     <div className="mb-6 w-full">
       <h2 className="text-[22px] sm:text-[24px] md:text-[25px] text-gray-500 mb-4 font-normal">
         {title}
       </h2>
+
       <div className="w-full md:overflow-x-auto sm:overflow-x-auto md:whitespace-nowrap">
         <div className="grid grid-cols-2 gap-y-4 md:flex md:space-x-4 sm:flex sm:space-x-4">
-          {renderContent()}
+          {(() => {
+            if (isError) {
+              return (
+                <ApiErrorNotice
+                  title="Failed to load books data"
+                  errors={[errorMessage]}
+                />
+              );
+            }
+
+            if (isLoading) {
+              return Array.from({ length: skeletonCount }).map((_, idx) => (
+                <div key={idx} className="flex-shrink-0">
+                  <Skeleton
+                    width={160}
+                    height={260}
+                    borderRadius={16}
+                    additionalClasses="p-4"
+                  />
+                </div>
+              ));
+            }
+
+            if (books.length === 0) {
+              return (
+                <ParagraphMessage
+                  text="No books available."
+                  className="mt-4 text-gray-600 col-span-full"
+                />
+              );
+            }
+
+            return books.map((book) => (
+              <div key={book.id} className="flex-shrink-0">
+                <BookItem {...book} onClick={handleBookClick} />
+              </div>
+            ));
+          })()}
         </div>
       </div>
     </div>
