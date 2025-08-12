e80d3c57bda83039f580e145356f8ad5fa6e6a74
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-08-12 15:30:44 +0700

    Rename file #160

--- practices/practice-two/src/helpers/__tests__/fileUtils.test.ts
+++ practices/practice-two/src/helpers/__tests__/fileUtils.test.ts
@@ -1,20 +1,20 @@
-import { readFileAsBase64 } from "../fileUtils";
+import { readFileAsBase64 } from "../file";
 
 describe("readFileAsBase64", () => {
-	it("should read file as base64 string", (done) => {
-		const file = new File(["Hello, World!"], "hello.txt", {
-			type: "text/plain",
-		});
+  it("should read file as base64 string", (done) => {
+    const file = new File(["Hello, World!"], "hello.txt", {
+      type: "text/plain",
+    });
 
-		const onSuccess = (base64: string) => {
-			try {
-				expect(base64).toMatch(/^data:text\/plain;base64,/);
-				done();
-			} catch (error) {
-				done(error);
-			}
-		};
+    const onSuccess = (base64: string) => {
+      try {
+        expect(base64).toMatch(/^data:text\/plain;base64,/);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    };
 
-		readFileAsBase64(file, onSuccess);
-	});
+    readFileAsBase64(file, onSuccess);
+  });
 });
