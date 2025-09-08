09ae16a2bac814ae382306d674806d8fe2e1f31c
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-09-08 16:47:43 +0700

    Highlight row & column header of selected cell #176

--- practices/practice-three/src/pages/SpreadsheetPage/index.tsx
+++ practices/practice-three/src/pages/SpreadsheetPage/index.tsx
@@ -1,6 +1,6 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useShallow } from "zustand/react/shallow";
-import Spreadsheet from "react-spreadsheet";
+import Spreadsheet, { RangeSelection, Selection } from "react-spreadsheet";
 
 // Components
 import { DraggableWindow } from "@/components";
@@ -10,6 +10,9 @@ import { WindowKeys } from "@/constant";
 
 // Store
 import { useWindowStore } from "@/stores";
+
+// Helpers
+import { removeSelectedClass } from "./helpers";
 
 const SpreadsheetPage = ({
   onClose,
@@ -22,6 +25,11 @@ const SpreadsheetPage = ({
   onMinimize: () => void;
   zIndex: number;
 }) => {
+  const [previousSelectedCell, setPreviousSelectedCell] = useState<{
+    row: number;
+    column: number;
+  } | null>(null);
+
   const { zIndexOrder, setZIndexOrder } = useWindowStore(
     useShallow((state) => ({
       zIndexOrder: state.zIndexOrder,
@@ -53,6 +61,45 @@ const SpreadsheetPage = ({
     []
   );
 
+  const handleSelectCell = (selected: Selection) => {
+    if (selected instanceof RangeSelection) {
+      const rowIndex = selected?.range?.start?.row;
+      const colIndex = selected?.range?.start?.column + 1;
+
+      const columnHeader = document.querySelector(
+        `.Spreadsheet__header:nth-child(${colIndex + 1})`
+      );
+      const rowHeader = document.querySelector(`tr[row="${rowIndex}"] th`);
+
+      // Remove "selected" class from previous cell
+      if (previousSelectedCell) {
+        removeSelectedClass(
+          previousSelectedCell.row,
+          previousSelectedCell.column
+        );
+      }
+
+      // Using setTimeout to ensure the DOM is updated before adding the class
+      // This is necessary when clicking on the intersection of column and row headers,
+      // and then clicking on a cell. This causes the DOM to change, so we need to wait
+      // until the DOM is fully updated before adding the class.
+      setTimeout(() => {
+        columnHeader?.classList.add("header_selected");
+        rowHeader?.classList.add("header_selected");
+      }, 0);
+
+      setPreviousSelectedCell({ row: rowIndex, column: colIndex });
+    } else {
+      // Remove the selection class if no selection
+      if (previousSelectedCell) {
+        removeSelectedClass(
+          previousSelectedCell.row,
+          previousSelectedCell.column
+        );
+      }
+    }
+  };
+
   return (
     <DraggableWindow
       src="/images/spreadsheet.png"
@@ -68,6 +115,7 @@ const SpreadsheetPage = ({
         columnLabels={columnLabels}
         rowLabels={rowLabels}
         onChange={(newData) => console.log(newData)}
+        onSelect={handleSelectCell}
       />
     </DraggableWindow>
   );
