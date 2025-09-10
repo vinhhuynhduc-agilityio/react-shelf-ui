fcd9419e87f2c65cad303ee9bb23adfbc5294678
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-09-10 11:17:41 +0700

    Add Spreadsheet Address Input and Header Highlighting #179

--- practices/practice-three/src/pages/SpreadsheetPage/helpers.ts
+++ practices/practice-three/src/pages/SpreadsheetPage/helpers.ts
@@ -1,3 +1,5 @@
+import { RangeSelection, Selection } from "react-spreadsheet";
+
 export const removeSelectedClass = (rowIndex: number, colIndex: number) => {
   const columnHeader = document.querySelector(
     `.Spreadsheet__header:nth-child(${colIndex + 1})`
@@ -6,4 +8,27 @@ export const removeSelectedClass = (rowIndex: number, colIndex: number) => {
 
   columnHeader?.classList.remove("header_selected");
   rowHeader?.classList.remove("header_selected");
+};
+
+export const getSelectedRange = (
+  selected: Selection,
+  columnLabels: string[],
+  rowLabels: string[]
+) => {
+  if (selected instanceof RangeSelection) {
+    const { start, end } = selected.range;
+    const startCol = columnLabels[start.column];
+    const startRow = rowLabels[start.row];
+    let rangeStr = `${startCol}${startRow}`;
+
+    if (start.row !== end.row || start.column !== end.column) {
+      const endCol = columnLabels[end.column];
+      const endRow = rowLabels[end.row];
+      rangeStr += `:${endCol}${endRow}`;
+    }
+
+    return rangeStr;
+  }
+
+  return "";
 };
--- practices/practice-three/src/pages/SpreadsheetPage/index.tsx
+++ practices/practice-three/src/pages/SpreadsheetPage/index.tsx
@@ -18,7 +18,7 @@ import { WindowKeys } from "@/constant";
 import { useWindowStore } from "@/stores";
 
 // Helpers
-import { removeSelectedClass } from "./helpers";
+import { getSelectedRange, removeSelectedClass } from "./helpers";
 
 const SpreadsheetPage = ({
   onClose,
@@ -31,7 +31,6 @@ const SpreadsheetPage = ({
   onMinimize: () => void;
   zIndex: number;
 }) => {
-  // State
   const [previousSelectedCell, setPreviousSelectedCell] = useState<{
     row: number;
     column: number;
@@ -42,6 +41,9 @@ const SpreadsheetPage = ({
       Array.from({ length: 12 }, () => ({ value: "" }))
     )
   );
+  const [selectedRange, setSelectedRange] = useState<string>("");
+
+  // Undo/Redo stacks
   const [undoStack, setUndoStack] = useState<Matrix<CellBase<string>>[]>([]);
   const [redoStack, setRedoStack] = useState<Matrix<CellBase<string>>[]>([]);
   const [pendingUndo, setPendingUndo] = useState<Matrix<
@@ -73,9 +75,14 @@ const SpreadsheetPage = ({
   };
 
   const handleSelectCell = (selected: Selection) => {
+    // highlight headers based on selection and update selectedRange state string for display input box
+    const rangeStr = getSelectedRange(selected, columnLabels, rowLabels);
+    setSelectedRange(rangeStr);
+
+    // highlight selected cell's row and column headers
     if (selected instanceof RangeSelection) {
-      const rowIndex = selected?.range?.start?.row;
-      const colIndex = selected?.range?.start?.column + 1;
+      const rowIndex = selected.range.start.row;
+      const colIndex = selected.range.start.column + 1;
 
       const columnHeader = document.querySelector(
         `.Spreadsheet__header:nth-child(${colIndex + 1})`
@@ -91,9 +98,6 @@ const SpreadsheetPage = ({
       }
 
       // Using setTimeout to ensure the DOM is updated before adding the class
-      // This is necessary when clicking on the intersection of column and row headers,
-      // and then clicking on a cell. This causes the DOM to change, so we need to wait
-      // until the DOM is fully updated before adding the class.
       setTimeout(() => {
         columnHeader?.classList.add("header_selected");
         rowHeader?.classList.add("header_selected");
@@ -165,7 +169,14 @@ const SpreadsheetPage = ({
       onMouseDown={handleMouseDown}
     >
       <div className="flex flex-col">
-        <div className="flex items-center border-b border-gray-300 p-1 space-x-1">
+        <div className="flex items-center border-b border-gray-300 p-1 space-x-1 bg-[#f4f4f4]">
+          <input
+            type="text"
+            value={selectedRange}
+            readOnly
+            className="border border-gray-300 p-1 bg-white w-24 rounded focus:outline-none ml-1"
+            placeholder="Select cell"
+          />
           <button
             onClick={handleUndo}
             className="p-2 rounded hover:bg-gray-200 transition-colors cursor-pointer"
