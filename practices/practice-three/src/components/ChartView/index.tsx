d8e348a912d0f1f103ab605b0865ef774e4f16a7
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-09-29 18:09:42 +0700

    Update color for legend and bar elements in the column chart #209

--- practices/practice-three/src/components/ChartView/index.tsx
+++ practices/practice-three/src/components/ChartView/index.tsx
@@ -1,11 +1,23 @@
 import { useMemo } from "react";
-import { Column } from "@ant-design/plots";
+import { Column, ColumnConfig } from "@ant-design/plots";
 
 // Helpers
 import { generateChartData } from "@/helpers";
 
 // Types
 import { Pivot } from "@/types";
+
+interface ChartData {
+  year: number | string;
+  type: string;
+  value: number;
+}
+
+interface ItemMarkerFill {
+  label: string;
+  color: string;
+  id: string;
+}
 
 interface ChartViewProps {
   pivot: Pivot[];
@@ -14,12 +26,28 @@ interface ChartViewProps {
 
 const ChartView = ({ pivot, height }: ChartViewProps) => {
   const data = useMemo(() => generateChartData(pivot), [pivot]);
-  const config = {
+  const colorMap: { [key: string]: string } = {
+    "oil (min)": "#ff4d4f",
+    "oil (sum)": "#722ed1",
+  };
+  const config: ColumnConfig = {
     data,
     xField: "year",
     yField: "value",
     seriesField: "type",
     colorField: "type",
+    legend: {
+      color: {
+        position: "right",
+        layout: {
+          justifyContent: "center",
+        },
+        itemMarkerFill: (type: ItemMarkerFill) => colorMap[type.label],
+      },
+    },
+    style: {
+      fill: (datum: ChartData) => colorMap[datum.type],
+    },
     height,
   };
   return <Column {...config} />;
