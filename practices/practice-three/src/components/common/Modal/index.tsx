6865c014201a5ee1c8c6c4390f18d3658907d48f
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-10-07 17:39:41 +0700

    Create common Modal component #218

---
+++ practices/practice-three/src/components/common/Modal/index.tsx
@@ -0,0 +1,69 @@
+import { ReactNode, useRef } from "react";
+import { createPortal } from "react-dom";
+import clsx from "clsx";
+
+// components
+import IconButton from "@/components/IconButton";
+
+interface ModalProps {
+  isOpen: boolean;
+  title: string;
+  onClose: () => void;
+  children: ReactNode;
+  className?: string;
+  headerHeight?: number;
+}
+
+const ModalContent = ({
+  isOpen,
+  title,
+  onClose,
+  children,
+  className = "",
+  headerHeight = 38,
+}: ModalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
+      {/* Overlay */}
+      <div className="fixed inset-0 bg-black/20" />
+      {/* Modal Content */}
+      <div
+        ref={modalRef}
+        className={clsx(
+          "relative overflow-hidden max-w-md w-full bg-[#FFFFFF] shadow-lg",
+          className
+        )}
+      >
+        {/* Header */}
+        <div
+          className={clsx(
+            "flex justify-between items-center ml-[17px] mt-[8px] mr-[20px]",
+            `h-[${headerHeight}px]`
+          )}
+        >
+          <h2 className="text-[#475466] font-medium text-[16px] truncate">
+            {title}
+          </h2>
+          <IconButton
+            icon="fa-solid fa-x fa-xs"
+            onClick={onClose}
+            iconStyles="text-[#94A1B3] hover:text-[#1CA1C1] text-lg leading-none px-3 py-4 rounded-full"
+          />
+        </div>
+
+        {/* Body */}
+        <div className="ml-[17px] mr-[17px] mb-[17px]">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+const Modal = (props: ModalProps) => {
+  return createPortal(<ModalContent {...props} />, document.body);
+};
+
+export default Modal;
