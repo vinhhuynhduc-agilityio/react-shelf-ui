f001bef0efe71c946fc625342329dd3a4ebb0e68
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-10-14 14:17:59 +0700

    Update constant for board and tasks

--- practices/practice-three/src/constant/routes.ts
+++ practices/practice-three/src/constant/routes.ts
@@ -1,4 +1,7 @@
 export const API_ENDPOINTS = {
   PIVOT: "/pivot",
   KANBANS: "/kanbans",
+  FILE_MANAGER: "/files",
+  TASKS: "/tasks",
+  BOARD: "/board",
 };
--- practices/practice-three/src/services/kanban.ts
+++ practices/practice-three/src/services/kanban.ts
@@ -19,27 +19,34 @@ import { API_BASE_URL } from "@/services";
 // Types
 import { BoardColumn, Task } from "@/types";
 
+// constant
+import { API_ENDPOINTS } from "@/constant";
+
 /* Tasks */
 export const getTasks = async (): Promise<Task[]> =>
-  apiRequest("GET", `${API_BASE_URL}/tasks`);
+  apiRequest("GET", `${API_BASE_URL}${API_ENDPOINTS.TASKS}`);
 
 export const addTask = async (task: Omit<Task, "id">): Promise<Task> =>
-  apiRequest("POST", `${API_BASE_URL}/tasks`, task);
+  apiRequest("POST", `${API_BASE_URL}${API_ENDPOINTS.TASKS}`, task);
 
 export const updateTask = async (task: Task): Promise<Task> =>
-  apiRequest("PUT", `${API_BASE_URL}/tasks/${task.id}`, task);
+  apiRequest("PUT", `${API_BASE_URL}${API_ENDPOINTS.TASKS}/${task.id}`, task);
 
 export const deleteTask = async (id: string): Promise<void> =>
-  apiRequest("DELETE", `${API_BASE_URL}/tasks/${id}`);
+  apiRequest("DELETE", `${API_BASE_URL}${API_ENDPOINTS.TASKS}/${id}`);
 
 /* Board */
 export const getBoard = async (): Promise<BoardColumn[]> =>
-  apiRequest("GET", `${API_BASE_URL}/board`);
+  apiRequest("GET", `${API_BASE_URL}${API_ENDPOINTS.BOARD}`);
 
 export const updateBoardColumn = async (
   column: BoardColumn
 ): Promise<BoardColumn> =>
-  apiRequest("PUT", `${API_BASE_URL}/board/${column.id}`, column);
+  apiRequest(
+    "PUT",
+    `${API_BASE_URL}${API_ENDPOINTS.BOARD}/${column.id}`,
+    column
+  );
 
 export const saveKanbanBoard = async ({
   board,
