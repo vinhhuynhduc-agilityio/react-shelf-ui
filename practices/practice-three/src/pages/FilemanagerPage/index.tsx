3a27e8f28d4570564154fb8e8840290a0ed0820d
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-10-17 17:46:40 +0700

    Generate a detail table when the user clicks on a navigation table item #228

--- practices/practice-three/src/pages/FilemanagerPage/index.tsx
+++ practices/practice-three/src/pages/FilemanagerPage/index.tsx
@@ -1,6 +1,7 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { Tree } from "antd";
 import type { DataNode } from "antd/es/tree";
+import type { ColumnsType } from "antd/es/table";
 import { useShallow } from "zustand/react/shallow";
 
 // constant
@@ -13,7 +14,7 @@ import { useWindowStore } from "@/stores";
 import { useFilemanagerQuery } from "@/hook";
 
 // components
-import { Button, DraggableWindow, IconButton } from "@/components";
+import { Button, DataTable, DraggableWindow, IconButton } from "@/components";
 
 // types
 import { FileItem } from "@/types";
@@ -29,6 +30,13 @@ const FilemanagerPage = ({
   onMinimize: () => void;
   zIndex: number;
 }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // state
+  const [tableHeight, setTableHeight] = useState<number>(0);
+  const [selectedFolder, setSelectedFolder] = useState<string>("root");
+
+  // store
   const { zIndexOrder, setZIndexOrder, isMinimized } = useWindowStore(
     useShallow((state) => ({
       zIndexOrder: state.zIndexOrder,
@@ -36,6 +44,33 @@ const FilemanagerPage = ({
       isMinimized: state.windows[WINDOW_KEYS.FILE_MANAGER].isMinimized,
     }))
   );
+
+  useLayoutEffect(() => {
+    const containerElement = containerRef.current;
+
+    const updateHeight = () => {
+      if (containerElement) {
+        const buttonBarHeight = 70;
+        const tableHeaderHeight = 37;
+        const availableHeight =
+          containerElement.clientHeight - tableHeaderHeight - buttonBarHeight;
+        setTableHeight(Math.max(availableHeight, 100));
+      }
+    };
+
+    const observer = new ResizeObserver(updateHeight);
+    if (containerElement) {
+      observer.observe(containerElement);
+    }
+
+    updateHeight();
+
+    return () => {
+      if (containerElement) {
+        observer.unobserve(containerElement);
+      }
+    };
+  }, []);
 
   const handleMouseDown = () => {
     if (zIndexOrder[zIndexOrder.length - 1] !== WINDOW_KEYS.FILE_MANAGER) {
@@ -69,7 +104,7 @@ const FilemanagerPage = ({
       {
         title: (
           <span>
-            <i className="fa-solid fa-folder fa-lg mr-5px] text-[#94A1B3]"></i>
+            <i className="fa-solid fa-folder fa-lg mr-[5px] text-[#94A1B3]"></i>
             My Files
           </span>
         ),
@@ -78,6 +113,61 @@ const FilemanagerPage = ({
       },
     ];
   }, [files, buildTree]);
+
+  const filteredItems = useMemo(() => {
+    return files
+      .filter((item) => item.parentId === selectedFolder)
+      .map((item) => ({
+        ...item,
+        key: item.id,
+      }));
+  }, [files, selectedFolder]);
+
+  const columns = useMemo<ColumnsType<FileItem>>(
+    () => [
+      {
+        title: "",
+        dataIndex: "name",
+        key: "name",
+        render: (text: string, record: FileItem) => (
+          <span>
+            <i
+              className={`fa-solid ${
+                record.type === "folder"
+                  ? "fa-folder text-[#1f88dd]"
+                  : "fa-file text-[#b3cae1]"
+              } fa-lg mr-[10px]`}
+            ></i>
+            {text}
+          </span>
+        ),
+      },
+      {
+        title: "Size",
+        dataIndex: "size",
+        key: "size",
+        width: 100,
+        align: "left",
+        render: (size: number) => `${size} KB`,
+      },
+      {
+        title: "Date",
+        dataIndex: "date",
+        key: "date",
+        width: 150,
+        align: "left",
+        render: () =>
+          new Date()
+            .toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })
+            .replace(/,/, ""),
+      },
+    ],
+    []
+  );
 
   return (
     <DraggableWindow
@@ -91,9 +181,12 @@ const FilemanagerPage = ({
       onMinimize={onMinimize}
       onMouseDown={handleMouseDown}
     >
-      <div className="flex flex-col h-full bg-[#EBEDF0]">
+      <div
+        className="flex flex-col h-full w-full overflow-hidden bg-[#EBEDF0]"
+        ref={containerRef}
+      >
         {/* Header */}
-        <div className="bg-[#FFFFFF] h-[56px] w-full box-content rounded-[2px] border border-[#DADEE0] text-[#475466] flex items-center">
+        <div className="flex items-center h-[56px] flex-shrink-0 w-full rounded-[2px] border border-[#DADEE0] text-[#475466] bg-[#FFFFFF] ">
           <div className="flex flex-1 items-center ml-[12px]">
             <span className="font-medium mr-4 text-[#475466]">Files</span>
             <div className="flex-1 max-w-[300px] min-w-[10px] flex items-center h-[32px] relative overflow-hidden">
@@ -113,7 +206,7 @@ const FilemanagerPage = ({
           </div>
           <div className="flex items-center space-x-1 mr-[12px]">
             <IconButton
-              buttonStyles="p-1 flex justify-center items-center w-[60px] h-[38px] bg-[#daddeb] hover:bg-[#E4E6F0]"
+              buttonStyles="p-1 flex justify-center items-center w-[60px] h-[38px] bg-[#F4F5F9] hover:bg-[#E4E6F0]"
               iconStyles="fa-solid fa-eye text-[#1CA1C1] text-sm"
               onClick={() => {}}
             />
@@ -126,18 +219,27 @@ const FilemanagerPage = ({
         </div>
         {/* Body */}
         <div className="flex flex-1 bg-[#EBEDF0]">
-          <div className="w-[250px] flex flex-col bg-[#FFFFFF] mt-[10px] mr-[10px] box-content rounded-[2px] border border-[#DADEE0] text-[#475466]">
+          <div className="w-[250px] flex flex-col bg-[#FFFFFF] mt-[10px] mr-[10px] rounded-[2px] border border-[#DADEE0] text-[#475466]">
             <Button variant="primary" className="mx-4 mt-[8px] mb-[8px]">
               Add New
             </Button>
             <Tree
               treeData={treeData}
-              onSelect={() => {}}
+              onSelect={(keys) => {
+                if (keys.length > 0) {
+                  setSelectedFolder(keys[0] as string);
+                }
+              }}
               defaultExpandedKeys={["root"]}
+              defaultSelectedKeys={["root"]}
             />
           </div>
-          <div className="flex-1 bg-[#FFFFFF] mt-[10px] box-content rounded-[2px] border border-[#DADEE0] text-[#475466]">
-            Details of Selected Folder
+          <div className="flex-1 bg-[#FFFFFF] mt-[10px] box-content rounded-[2px] border border-[#e0dada] text-[#475466] overflow-auto">
+            <DataTable
+              columns={columns}
+              dataSource={filteredItems}
+              tableHeight={tableHeight}
+            />
           </div>
         </div>
       </div>
