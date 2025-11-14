6d82f006965cba171d5cb260f5c3a38b6caf81b7
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-11-14 16:40:48 +0700

    Spreadsheet: update toolbar UI options #251

---
+++ practices/practice-three/src/constant/spreadsheet.ts
@@ -0,0 +1,30 @@
+export const toolbarItems = [
+  // --- Undo / Redo ---
+  "undo",
+  "redo",
+  "|",
+
+  // font family & size
+  "font",
+  "font-size",
+  "|",
+
+  // --- Text Style ---
+  "bold",
+  "italic",
+  "underline",
+  "strike-through",
+
+  // --- Colors & Border & Merge ---
+  "font-color",
+  "background",
+  "border",
+  "|",
+
+  // --- Alignment ---
+  "horizontal-align",
+  "vertical-align",
+  "text-wrap",
+  "merge-cell",
+  "|",
+];
