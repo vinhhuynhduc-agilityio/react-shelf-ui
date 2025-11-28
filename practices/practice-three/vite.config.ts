284b3ecbd09a5c63dabdc6934ad9d7171244f83f
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2025-11-28 14:35:47 +0700

    update vite

--- practices/practice-three/vite.config.ts
+++ practices/practice-three/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import preload from "vite-plugin-preload";
 import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), preload()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Base alias for `src`
+      "@": path.resolve(__dirname, "src"),
     },
   },
   esbuild: {
